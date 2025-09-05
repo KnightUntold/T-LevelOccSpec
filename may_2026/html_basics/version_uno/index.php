a429afb84d401f92b51c7a2727dc48c110a2c80f
KnightUntold <summerwall86x@gmail.com>
2025-09-05 13:03:23 +0100

    commit of work before lunch

---
+++ may_2026/html_basics/version_uno/index.php
@@ -0,0 +1,52 @@
+<?php //this lets you code in ph
+//have to suffer through echo hell because of it system being shit
+echo "<!DOCTYPE html>"; //declares html - correct structure
+
+echo "<html>";
+
+    echo "<head>";
+       echo "<title>it is i </title>";
+    echo "</head>";
+
+    echo "<body>";
+        echo "<h1> this echo nonsense is hurting me mentally</h1>";
+        echo "<hr>"; //adds a line and 2 whitespace above and below
+        echo "<p>text</p>"; //paragraphs
+        echo "</br>"; //break tags
+
+        echo "<em>text</em>"; //italics
+        echo "</br>";
+        echo "<i>text</i>"; //italics
+        echo "</br>";
+
+        echo "<u>text</u>"; //underline
+        echo "</br>";
+
+        echo "<b>text</b>"; //bold
+        echo "</br>";
+        echo "<strong>text</strong>"; //bold
+        echo "</br>";
+
+        echo "<a href = 'page.php'>text</a>"; //href links
+        echo "</br>";
+
+        echo "<table>"; //table
+            echo "<tr>"; //table row
+                echo "<td>text</td>"; //table row items
+                echo "<td>text</td>";
+                echo "<td>text</td>";
+            echo "</tr>";
+            echo "<tr>";
+                echo "<td>text</td>";
+                echo "<td>text</td>";
+                echo "<td>text</td>";
+            echo "</tr>";
+        echo "</table>";
+
+        echo "</br>";
+        echo "<img src = 'glados_potatOS.jpg'>"; //image
+    echo "</body>";
+
+echo "</html>";
+
+?>
