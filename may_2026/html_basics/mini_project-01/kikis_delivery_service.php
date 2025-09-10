5b1070107b78a940d3928e571431e51d9a36de96
KnightUntold <summerwall86x@gmail.com>
2025-09-10 18:30:29 +0100

    added basic html setup to all pages

--- may_2026/html_basics/mini_project-01/kikis_delivery_service.php
+++ may_2026/html_basics/mini_project-01/kikis_delivery_service.php
@@ -1,1 +1,18 @@
 <?php
+    include 'header.php';
+
+    echo "<!DOCTYPE html>";
+    echo "<html lang='en'>";
+        echo "<head>";
+            echo "<meta charset='UTF-8'>";
+            echo "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
+            echo "<link rel='stylesheet' type='text/css' href='styles.css'>";
+            echo "<title>Kiki's Delivery Service</title>";
+        echo "</head>";
+        echo "<body>";
+
+        echo "</body>";
+    echo "</html>";
+
+    include 'footer.php';
+?>
