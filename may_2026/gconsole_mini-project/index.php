5dbc2771b51dda5cfbea435d54986ab3c3339b3c
KnightUntold <summerwall86x@gmail.com>
2025-09-30 09:12:09 +0100

    organising github

--- may_2026/gconsole_mini-project/index.php
+++ may_2026/gconsole_mini-project/index.php
@@ -15,7 +15,7 @@
         echo "<head>";
             echo "<meta charset='UTF-8'>";
             echo "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
-            echo "<link rel='stylesheet' type='text/css' href='../../reuseable_code/ezstyles.css'>";
+            echo "<link rel='stylesheet' type='text/css' href='../reuseable_code/ezstyles.css'>";
             echo "<title>Home</title>";
         echo "</head>";
         echo "<body class='back_powderblue'>";
