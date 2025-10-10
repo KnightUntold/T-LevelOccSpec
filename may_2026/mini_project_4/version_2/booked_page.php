b314a869e22c37586631630ce9df95040b0d3f1b
KnightUntold <summerwall86x@gmail.com>
2025-10-10 13:53:25 +0100

    commiting files to version 2 because i forogt to like an idiot

---
+++ may_2026/mini_project_4/version_2/booked_page.php
@@ -0,0 +1,23 @@
+<?php
+    echo "<!DOCTYPE html>";
+
+        echo "<html lang='en'>";
+
+        echo "<head>";
+            echo "<meta charset='UTF-8'>";
+            echo "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
+            echo "<link rel='stylesheet' type='text/css' href='../../reuseable_code/ezstyles.css'>";
+            echo "<title>Appointment Booked Successfully</title>";
+        echo "</head>";
+
+        echo "<body>";
+
+            require "header.php";
+
+            echo "<h1 class='center'>Appointment Booked for [date and time here]</h1>";
+
+            require "footer.php";
+
+        echo "</body>";
+
+    echo "</html>";
